83e8c1863f24311f803301120d2b31bc052685bd
Adam Robins <iam@adamrobins.com>
2024-01-07 14:20:12 +0100

    POC Progress - Required update on dep lib irc-framework - added patch request

--- config.ts
+++ config.ts
@@ -1,11 +1,13 @@
 const UserSettings = {
     // Listed as BOT but is actually a user
     // each "Setting" is a nice that will sit on the server and proxied through the websocket. 
-    nick : 'bot_test',
-    password : 'nickPass',
-    realname : 'bnc_test',
+    nick : 'bot',
+    username: 'El encuentro',
+    gecos: 'El encuentro',
+    password : 'botpass',
+    realname : 'El encuentro',
     channels : [{name: 'bot_test', key: ""}],
-    host : 'irc.freenode.net',
+    host : 'irc.bot.net',
     port : 6667,
     auto_reconnect: false,
     auto_reconnect_wait: 10000,
--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ import winston from 'winston';
 // TODO: Write typings for this.
 import { Client } from 'irc-framework';
 import UserSettings from './config';
-import { stat } from 'fs';
+import { cli } from 'winston/lib/winston/config';
 
 const app = Express();
 
@@ -20,21 +20,22 @@ app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+let socketConnections = [];
+
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     defaultMeta: { service: 'irc' },
     transports: [
-      //
-      // - Write all logs with importance level of `error` or less to `error.log`
-      // - Write all logs with importance level of `info` or less to `combined.log`
-      //
-      // new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      // new winston.transports.File({ filename: 'combined.log' }),
-      new winston.transports.Console()
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        // new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.Console()
 
     ],
-  });
+});
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
@@ -46,14 +47,14 @@ const io = new Server(httpServer, {
 
 const client = new Client();
 
-app.use('/static', Express.static('public'))
+app.use('/static', Express.static('public'));
 
 app.get('/', (req, res) => {
     if (!client.connected)
         client.connect(UserSettings);
 
     client.on('socket close', (e) => {
-        
+
         console.log(e);
         console.log('socket closed');
     });
@@ -68,8 +69,8 @@ app.get('/', (req, res) => {
 
     client.on('message', (event) => {
         // TODO: This is for the POC - this should have a DTO and also is XSS vulnerable.
-        logger.info({user: event.nick, channel: event.target, message: event.message} );
-        io.emit('chat:message', {user: event.nick, channel: event.target, message: event.message});
+        logger.info({ user: event.nick, channel: event.target, message: event.message });
+        io.emit('chat:message', { user: event.nick, channel: event.target, message: event.message });
     });
 
     client.on('error', (event) => {
@@ -80,41 +81,68 @@ app.get('/', (req, res) => {
 });
 
 app.get('/channel/joinall', (req, res) => {
-
     var result = [];
 
-    for (let channel of UserSettings.channels) {
-        var channelObj = client.channel('#' + channel.name, channel.key );
+    UserSettings.channels.forEach(channel => {
+        var channelObj = client.channel('#' + channel.name, channel.key);
         channelObj.join();
-        result.push({channel: channel.name, status: "joined"});
-    }
-    res.send(result);
+        channelObj.updateUsers()
+        
+        channelObj.updateUsers(function() {
+            var users = channelObj.users;
+            console.log(users);
+            result.push({ channel: channel.name, status: "joined", users: users });
+
+            res.send(result);
+        });
+    
+        // Or you could even stream the channel messages elsewhere
+        // var stream = channel.stream();
+        // stream.pipe(process.stdout);
+    });
 });
 
 app.get('/channel/join/:channelName', (req, res) => {
     // TODO: Do we always need o build out a buffer ?
-    
-    var channel = client.channel('#'+req.params.channelName, );
-    channel.join();
+    // get chanel from UserSettings
+    // var channelInfo = UserSettings.channels.find(x => x.name == req.params.channelName);
 
-    res.send('Joined channel : #' + req.params.channelName);
+    var channel = client.channel('#' + req.params.channelName);
+    channel.join();
+    
+    channel.updateUsers(function() {
+        var users = channel.users;
+        socketConnections.forEach(socket => {
+            socket.emit('channel:joined', { channel: req.params.channelName, users: users });
+        });
+    
+        res.send('Joined channel : #' + req.params.channelName);
+    });
 });
 
 app.get('/channel/list', (req, res) => {
     res.send(client.channelList);
 });
 
-app.post('/message', (req, res) => {
+app.post('/message/send', (req, res) => {
     console.log(req.body);
-    client.say('#channel', req.body.message);
+    client.say('#' + req.body.channel, req.body.message);
+    client.say(req.body.channel, req.body.message);
     res.send('Message sent!');
 });
 
+// Socket Hooks
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('message', (message) => {
-        console.log(message);
-        client.say('#channel', message);
+    // TODO: add by userID later
+    socketConnections.push(socket);
+    socket.on('client:message', (message) => {
+        var channel = "tadas_test";
+        if(!message.channel) channel = message.channel;
+        client.say('#' + channel, message.message);
+        // client.say(channel, message.message);
+        // Shadow message to self.
+        // client.say('tadas', message.message);
     });
 });
 
